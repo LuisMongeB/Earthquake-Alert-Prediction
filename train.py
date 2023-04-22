aa49668ad9506b75c4894b24391a1fba26ba518b
Luis <luigimonge16@gmail.com>
2023-04-23 00:16:00 +0200

    Add data preprocessing functions

---
+++ train.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+from preparation import prepare_data
+
+
+def split_data(df, ratio=(0.7, 0.2, 0.1), random_state=42):
+    """
+    Splits a pandas dataframe into train, validation, and test sets based on the provided ratio.
+
+    Returns:
+        Tuple of three dataframes: (train_df, val_df, test_df)
+    """
+    assert round(sum(ratio), 0) == 1.0, "Split ratio must add up to 1"
+
+    # Split the data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(df.drop('alert', axis=1),
+                                                        df['alert'],
+                                                        test_size=ratio[2],
+                                                        random_state=random_state)
+
+    # Split the remaining data into train and validation sets
+    val_ratio = ratio[1] / (ratio[0] + ratio[1])
+    X_train, X_val, y_train, y_val = train_test_split(X_train,
+                                                      y_train,
+                                                      test_size=val_ratio,
+                                                      random_state=random_state)
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+def encode_categorical_features(df, encoder):
+    """
+    Encode categorical features.
+    """
+    # Encode categorical features
+    alert_enc = pd.DataFrame({'alert': encoder.fit_transform(df['alert'])})
+    tsunami_enc = pd.DataFrame(
+        {'tsunami': encoder.fit_transform(df['tsunami'])})
+    cdi_enc = pd.get_dummies(
+        df['cdi'], prefix='cdi', drop_first=True).reset_index(drop=True)
+
+    # join both dataframes
+    cat_df = pd.concat([cdi_enc, tsunami_enc, alert_enc], axis=1)
+
+    return cat_df
+
+def scale_numerical_features(df, scaler):
+    """
+    Drops categorical variables, selects numerical ones returns scaled dataframe.
+    """
+    num_df = df.select_dtypes(include=np.number).drop(
+        ['tsunami', 'cdi'], axis=1)
+    scaled_features = scaler.fit_transform(num_df)
+    num_df = pd.DataFrame(scaled_features, columns=num_df.columns)
+
+    return num_df
+
+
+def preprocess(df):
+    """
+    Preprocess data for training.
+    1. Split data into train, validation, and test sets
+    2. Encode categorical features
+    3. Scale numerical features
+    """
+    # Encode categorical features
+    cat_df = encode_categorical_features(df, LabelEncoder())
+    num_df = scale_numerical_features(df, StandardScaler())
+    preprocessed = pd.concat([num_df, cat_df], axis=1)
+
+    return preprocessed
+
+
+def train_and_predict(split, model):
+    """
+    Train a model on a split and return the predictions.
+    """
+    # Split the data
+    X_train, y_train = split.drop('alert', axis=1), split['alert']
+
+    # Fit the model
+    model.fit(X_train, y_train)
+
+    # Predict on the validation set
+    preds = model.predict(X_train)
+
+    return preds
+
+
+# Load the data
+if __name__ == '__main__':
+
+    earthquakes_path = Path.cwd() / 'data' / 'earthquake-2001-2023.csv'
+
+    features_to_remove = ['title', 'date_time', 'magType', 'latitude',
+                          'longitude', 'location', 'continent', 'country']
+
+    earthquakes = prepare_data(earthquakes_path, features_to_remove)
+
+    print(earthquakes.head())
+
+    # Preprocess data
+    preprocessed = preprocess(earthquakes)
+
+    # split the data: X_train = train_df
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(preprocessed)
+    
+    # Define a list of models
+    models = [LogisticRegression(), RandomForestClassifier(),
+              DecisionTreeClassifier()]
+
+# Loop through the models and print the accuracy
+# for model in models:
+#     preds = train_and_predict(train_df, model)
+#     print(f"{model.__class__.__name__} accuracy: {accuracy_score(train_df['alert'], preds)}")
